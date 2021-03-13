b4e428ae45a013b5f3e434306b7832c9e427d303
realonereal <erohinreal@gmail.com>
2021-03-13 09:31:16 +0300

    add:dirty solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+ if(n > 0) {
+     let number = String(n);
+     return +number.split('').reverse().join('');
+ } else {
+     let number = String(-n);
+     return +number.split('').reverse().join('');
+
+ }
+
 }
